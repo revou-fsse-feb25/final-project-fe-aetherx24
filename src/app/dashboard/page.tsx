bb8cab5ae47a9e4db30ef72b16603fadb5fe65ca
Aetherx24 <iqbalfahrur@gmail.com>
2025-08-22 18:41:57 +0700

    feat: removed authguard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,5 @@
 import DashboardLayout from '@/components/DashboardLayout';
-import AuthGuard from '@/components/AuthGuard';
 
 export default function DashboardPage() {
-  return (
-    <AuthGuard>
-      <DashboardLayout />
-    </AuthGuard>
-  );
+  return <DashboardLayout />;
 }
